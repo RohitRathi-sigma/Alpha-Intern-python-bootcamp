aca88d7114d36bfb2f8985eb78635eb6eecb97b9
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-05 18:12:47 +0530

    Largest in three number

---
+++ DAY_3 conditional/largestInThreeNum.py
@@ -0,0 +1,16 @@
+#Write a program that takes three numbers as input and prints the largest number among them.
+
+def find_largest_number(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1  
+    elif num2 >= num1 and num2 >= num3:
+        return num2  
+    else:
+        return num3  
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+
+largest_number = find_largest_number(number1, number2, number3)
+print(f"The largest number among {number1}, {number2}, and {number3} is: {largest_number}")
