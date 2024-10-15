8ea2b87d764ce4b7b87a285e7c65301ca3d29794
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-15 20:36:59 +0530

    Reverse the string

---
+++ Assignment on String/reverseAString.py
@@ -0,0 +1,8 @@
+#Write a Python program to reverse a string?
+
+def reverse_string(s):
+    return s[::-1]
+
+input_string = "Alpha Intern"
+result = reverse_string(input_string)
+print("Reversed string:", result)
