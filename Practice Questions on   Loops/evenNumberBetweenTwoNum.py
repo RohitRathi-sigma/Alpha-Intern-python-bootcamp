dfe6b3b7f57860647c5ee204bff7c1237d1e6da4
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-17 22:10:53 +0530

    Even number

---
+++ Practice Questions on   Loops/evenNumberBetweenTwoNum.py
@@ -0,0 +1,6 @@
+#Write a Python program to print all the even numbers between 1 and 20 using a for loop and the range function.
+
+print("Even numbers between 1 and 20:")
+for num in range(2, 21, 2):
+    print(num, end=' ')
+print()  # For a newline
