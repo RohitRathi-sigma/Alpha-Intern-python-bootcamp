beaaba9dfc66ac3965a1ea093907242f053acdb6
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-17 22:13:15 +0530

    Square of numbers

---
+++ Practice Questions on   Loops/squareOfNumbers.py
@@ -0,0 +1,5 @@
+#Write a Python program to print the squares of all the numbers from 1 to 10 using a for loop and the range function.
+    
+print("Squares of numbers from 1 to 10:")
+for num in range(1, 11):
+    print(f"{num}^2 = {num ** 2}")
